f8dc8f07d04b7f70460f8addce0a86f9bb18694f
youichi Ishigaki <deepestred1968@gmail.com>
2021-10-18 21:32:36 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from "react";
+
 function App() {
-  return <h1>Hello World</h1>;
+  // const greeting = "Hi! Tom!"
+  // const dom = <h1 className="foo">{greeting} what's up?</h1>
+  // const inputDom = <input type="text" onChange = {()=>{console.log()}}/>
+  
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">
+        bar
+      </label>
+      <input type="text" onChange = {()=>{console.log("change")}}/>
+    </React.Fragment>
+  );
 }
-
-// import React,{Component} from "react";
-
-// function App() {
-//   return React.createElement(
-//     "div",
-//     null,
-//     "Hello World"
-//   );
-// }
 
 export default App;
