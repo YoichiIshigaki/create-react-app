90d37b0221e59ba1a7e72e5e903b76f8266c1272
youichi Ishigaki <deepestred1968@gmail.com>
2021-10-20 22:15:40 +0900

    function components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React,{Component} from "react";
 
-function App() {
-  // const greeting = "Hi! Tom!"
-  // const dom = <h1 className="foo">{greeting} what's up?</h1>
-  // const inputDom = <input type="text" onChange = {()=>{console.log()}}/>
+// function App() {
+//   // const greeting = "Hi! Tom!"
+//   // const dom = <h1 className="foo">{greeting} what's up?</h1>
+//   // const inputDom = <input type="text" onChange = {()=>{console.log()}}/>
   
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">
+//         bar
+//       </label>
+//       <input type="text" onChange = {()=>{console.log("aaa")}}/>
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">
-        bar
-      </label>
-      <input type="text" onChange = {()=>{console.log("change")}}/>
-    </React.Fragment>
-  );
+    <div>
+      <p>Hi</p>
+      <Cat/>
+      <Cat/>
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>neno</div>
 }
 
 export default App;
