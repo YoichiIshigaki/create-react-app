ce62b58547c097056e9c70512a8960fb974fec88
youichi Ishigaki <deepestred1968@gmail.com>
2021-10-21 00:02:14 +0900

    prop types

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React,{Component} from "react";
-
+import React from "react";
+import PropTypes from "prop-types"
 // function App() {
 //   // const greeting = "Hi! Tom!"
 //   // const dom = <h1 className="foo">{greeting} what's up?</h1>
 //   // const inputDom = <input type="text" onChange = {()=>{console.log()}}/>
-  
+
 //   return (
 //     <React.Fragment>
 //       <label htmlFor="bar">
@@ -16,17 +16,38 @@ import React,{Component} from "react";
 // }
 
 const App = () => {
-  return (
-    <div>
-      <p>Hi</p>
-      <Cat/>
-      <Cat/>
-    </div>
-  )
+	const profiles = [
+		{ name: "Taro", age: 10 },
+		{ name: "Hanako", age: 13 },
+		{ name: "Hanako" },
+	];
+
+	return (
+		<div>
+			{profiles.map((profile, index) => {
+				return <User name={profile.name} age={profile.age} key={index} />;
+			})}
+			<p>Hi</p>
+		</div>
+	);
+};
+
+const User = (props) => {
+	return (
+		<div>
+			<p>Hi! , I'am {props.name}.</p>
+			<p>I'm {props.age} years old.</p>
+		</div>
+	);
+};
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
-const Cat = () => {
-  return <div>neno</div>
-}
+User.defaultProps = {
+	age: 20,
+};
 
 export default App;
