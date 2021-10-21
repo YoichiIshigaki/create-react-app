daed1ecd4b7804b86e9a90bb369ff679c7feb690
youichi Ishigaki <deepestred1968@gmail.com>
2021-10-21 23:36:52 +0900

    midify

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
-import reducer from "./components/reducers";
-import App from "./App";
+import reducer from "./reducers";
+import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
 const store = createStore(reducer);
